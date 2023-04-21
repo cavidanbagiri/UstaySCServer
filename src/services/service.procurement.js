e70bc25b57db806850b0e7a819921a8a8204380a
cavidanbagiri <cavidanbagiri@gmail.com>
2023-04-21 11:31:27 +0300

    Change Query string in waiting mtf

--- src/services/service.procurement.js
+++ src/services/service.procurement.js
@@ -8,7 +8,8 @@ class ProcurementService {
     // Get Waiting MTF From MTF Tables
     static async getWaitingMTF(){
 
-        const string_query = `select * from mtfs where cond='Wait'`;
+        const string_query = `select m.*, u.username from mtfs m left join users u on m."UserModelId"=u.id where cond='Wait'`
+
         const result = await db.sequelize.query(string_query);
 
         return result[0];
