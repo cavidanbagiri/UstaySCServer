ce4e4a5949b41fc54ff9a8e002db02ab8d4ae7fb
cavidanbagiri <cavidanbagiri@gmail.com>
2023-07-10 23:05:32 +0300

    Add STF search type

--- src/services/service.order.js
+++ src/services/service.order.js
@@ -208,8 +208,11 @@ class OrderService {
       if (filtered_object[key] !== "") {
 
         // If Key name is material name or material type, like query will work will start wil entering value
-        if(key === 'material_name' || key === 'stf_num' ){
+        if(key === 'material_name' ){
           where_query += `${key} LIKE '${filtered_object[key]}%' `
+        }
+        else if(key === 'stf_num'){
+          where_query += `${key} LIKE '%${filtered_object[key]}' `
         }
         else if(key === 'created_at' ){
           where_query += `stfs.${key} = '${filtered_object[key]}' `
@@ -217,14 +220,12 @@ class OrderService {
         else{
           where_query += `${key} = '${filtered_object[key]}' `
         }
-
         where_query += 'and ';
       }
     }
 
     // After Adding Each Filtered Key, Functions add and operator and at the end, and operations will be removed 
     where_query = where_query.slice(0,-4);
-
     return where_query;
 
   }
